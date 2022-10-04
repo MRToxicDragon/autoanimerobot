93330e8b482b6263f1e79a39d9e6634141caf6fd
MRToxicDrgon <98029638+MRToxicDragon@users.noreply.github.com>
2022-10-04 13:47:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("19134212"))
+API_HASH = getenv("0d85b66bf650d9d64ec178e51fa6b2c9")
+BOT_TOKEN = getenv("5756094413:AAH5xI_BxmAduPJtbkdam1Q9Ku743XtreHs")
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("mongodb+srv://1:1@cluster0.1tetwnx.mongodb.net/?retryWrites=true&w=majority")
 
-INDEX_ID = int(getenv("INDEX_ID"))
-UPLOADS_ID = int(getenv("UPLOADS_ID"))
+INDEX_ID = int(getenv("-1001247900358"))
+UPLOADS_ID = int(getenv("-1001640403011"))
 
-STATUS_ID = int(getenv("STATUS_ID"))
-SCHEDULE_ID = int(getenv("SCHEDULE_ID"))
+STATUS_ID = int(getenv("https://t.me/ARKJ360_anime_db/3"))
+SCHEDULE_ID = int(getenv("https://t.me/ARKJ360_anime_db/5"))
 
-CHANNEL_TITLE = getenv("CHANNEL_TITLE")
-INDEX_USERNAME = getenv("INDEX_USERNAME")
-UPLOADS_USERNAME = getenv("UPLOADS_USERNAME")
+CHANNEL_TITLE = getenv("ARKJ360")
+INDEX_USERNAME = getenv("https://t.me/ARKJ360_anime_index")
+UPLOADS_USERNAME = getenv("https://t.me/ARKJ360_anime_db")
